915243f48fa3645bd342b5096f65c37008230c2a
hiranyaingle21-prog <hiranyaingle21@gmail.com>
2026-08-31 19:11:27 +0530

    C PROGRAM

---
+++ HI 3.c
@@ -0,0 +1,11 @@
+//Program 3: Write a program to calculate area of circle
+ Predefined values
+#include<stdio.h>
+ int main()
+{
+    float radius=1.1;
+    float area;
+    area=(3.14*radius*radius);
+    printf("\n Area of circle=%f",area);
+    return 0;
+}
