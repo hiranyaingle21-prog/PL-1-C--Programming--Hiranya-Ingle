40a416c44ca76c4cd3cc5900a4e6e892d5c8a093
hiranyaingle21-prog <hiranyaingle21@gmail.com>
2026-08-31 19:12:21 +0530

    C PROGRAM

---
+++ HI 5.c
@@ -0,0 +1,12 @@
+// Program 5: Write a program to find average of three numbers.
+   Predefined values
+#include<stdio.h>
+int main()
+{
+    float radius,area;
+    printf("\nEnter radius:");
+    scanf("%f",&radius);
+    area=(3.14*radius*radius);
+    printf("\nArea=%f",area);
+    return 0;
+}
